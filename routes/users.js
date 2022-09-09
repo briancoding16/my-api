e475128b3f6ae1379ed94163eebe23e8f4052500
unknown <16tupac16@gmail.com>
2022-09-09 19:10:41 -0300

    crud

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const servicesUsers = require('../services/servicesUsers')
 const router = express.Router()
+const validatorHandler = require('../middleware/validator.hendler')
+const { schemaProductCreate } = require('../schema/schemaProduct')
 
 router.get('/', async (req, res, next)=> {
  try {
@@ -11,5 +13,53 @@ router.get('/', async (req, res, next)=> {
  }
 })
 
+router.get('/:id',
+  async (req, res, next)=> {
+  try {
+   const {id} = req.params
+   const oneUser = await servicesUsers.findOne(id)
+   res.json(oneUser)
+  } catch (error) {
+   next(error)
+  }
+ })
+
+
+
+
+
+router.post('/', async(req, res, next)=>{
+  try {
+    const body = req.body
+    const newUser = await servicesUsers.createUser(body)
+    return {newUser}
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+router.patch('/:id', async(req, res, next)=>{
+try {
+  const {id} = req.params
+  const body = req.body
+  const updateUser = await servicesUsers.updateUser(id, body)
+  return res.json(updateUser)
+} catch (error) {
+  next(error)
+}
+})
+
+
+
+router.delete('/:id', async (req, res, next)=>{
+  try {
+    const {id} = req.params
+    const userDelete = await servicesUsers.deleteUser({id})
+    return res.json(userDelete)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
--- services/servicesUsers.js
+++ services/servicesUsers.js
@@ -1,13 +1,77 @@
+
+const { boom } = require('@hapi/boom')
 const getConnection = require('../libs/postgres')
 const {models} = require('../libs/sequelize')
 
 
-const getAllUsers = async (req, res) =>{
+const getAllUsers = async () =>{
   const response = await models.User.findAll()
   return response
 }
 
+const findOne = async (id)=>{
+ try {
+  const user = await models.User.findByPk(id)
+  if (!user) {
+    throw boom.notFound('user not found')
+  }
+  return user
+ } catch (error) {
+  console.log(error)
+ }
+}
+
+
+const createUser = async (body) => {
+  try {
+    console.log(body)
+    const newUser = await models.User.create(body)
+    console.log(newUser)
+    return {
+      user: newUser,
+      message: 'User created'
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const updateUser = async (id, body)=>{
+  try {
+    const user = await models.User.findByPk(id)
+    if (!user) {
+      return {
+        error: 'user not found'
+      }
+    }
+    const response = await user.update(body)
+    return {response}
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+const deleteUser = async (id)=>{
+  try {
+    const user = await models.User.findOne(id)
+    await user.destroy()
+    return {
+      message: 'User delete',
+      id
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  findOne
+
 }
